ccc38db26ac19ede1f3f4aa285d62edd02b23738
Aaron <arnhsmith@gmail.com>
2023-07-27 14:46:49 -0400

    checked and code passed all tests

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,16 @@
 from vehicle import Vehicle
 
+
 class Car(Vehicle):
-    pass
+
+    # how to use the method from the Vehicle class from vehicle.py file
+
+    # def gogo(self):
+    #     Vehicle.go()
+    
+    def go():
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+car = Car(5, 4)
+print(car.go())
